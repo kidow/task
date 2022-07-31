14cc4166e734c996f57759df7e35ee6446c3b8d4
kidow <39021158+kidow@users.noreply.github.com>
2022-07-31 14:30:48 +0900

    typescript

---
+++ pages/_document.tsx
@@ -0,0 +1,47 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import type { DocumentContext } from 'next/document'
+import { Children } from 'react'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: Children.toArray(initialProps.styles)
+    }
+  }
+  render() {
+    return (
+      <Html lang="ko" dir="ltr">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#475569" />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="icon"
+            type="image/png"
+            href="favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="favicon-16x16.png"
+            sizes="16x16"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+          />
+          <meta name="msapplicatqion-TileColor" content="#475569" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
